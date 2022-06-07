64e4d3976e058812d5a3857b9bdc65658db438b5
Calquito <39011234+Calquito@users.noreply.github.com>
2022-06-07 15:35:44 -0600

    Receive server

--- Servidor/server.js
+++ Servidor/server.js
@@ -1,55 +1,18 @@
-var express = require("express");
-var cors = require('cors');
-var corsMiddleware = require('./cors/cors');
-
-//Variables requeridas para el HTTPS
-const fs = require('fs')
-const https = require('https')
-const path = require('path')
-const port = 3000
-
-//Asignacion del express
-var app = express();
-
-//Definiciones para el CORS
-app.use(express.json({ limit: "100mb" }));
-app.options('*', corsMiddleware);
-app.use(corsMiddleware);
-app.use(cors());
-
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('Hello World!')
-})
-
-app.get('/', (req, res) => {
-  console.log(req.body)
-  res.send([{
-codigo : "CE-1209",
-nombre : "Algoritmos y estructuras de datos I",
-creditos : "4",
-profesor : "Isaac Ramirez Herrera",
-grupo : "1"
-
-},
-{
-codigo : "CE-5303",
-nombre : "Desarrollo de emprendedores ",
-creditos : "4",
-profesor : "Rita Picado Arroyo",
-grupo : "1"
-
-}
-
-])
-})
-
-//codigo hhtps
-https.createServer({
-    cert: fs.readFileSync('./ssl/server.crt'),
-    key: fs.readFileSync('./ssl/server.key')
-  }, app).listen(port, function () {
-    console.log(`Node server running on https://localhost:${port}`);
+const express = require("express");
+const bodyParser = require("body-parser")
+  
+// New app using express module
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+  
+  
+app.post("/", function(req, res) {
+  console.log(req.body);
+  res.send("Hello world!");
 });
-
-module.exports = app;
+  
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+})
